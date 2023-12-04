aa25e4cc281df0899cb7638dd427aa7d3a2eaa3b
jian0211 <myunggeun222@gmail.com>
2023-12-04 19:50:43 +0900

    소수판정함수작성

---
+++ math/prime/prime.js
@@ -0,0 +1,13 @@
+
+/** 
+ * 소수인지 판정하는 함수
+ * 주어진 타겟 값이 1인 경우, 소수가 아니다. 
+ * 시간복잡도는 O(루트N) 
+*/
+const isPrime = (target) => {
+    if(target === 1) return false;
+    for (let i = 2; i * i <= target; i++) {
+        if(target % i === 0) return false;
+    };
+    return true;
+};
