5871bebec9db6387f6a1dc7185fe088e5aabef2d
jian0211 <myunggeun222@gmail.com>
2023-12-05 21:48:10 +0900

    prime-10434:문제 해결 상태 아직 아님. 채점결과 오류 발생! 원인불명

---
+++ math/prime/prime10434.js
@@ -0,0 +1,56 @@
+/**
+ * https://www.acmicpc.net/problem/10434
+ *  
+ * 7은 분명 소수이다. 과연 행복할까?
+ *  7 → 72 = 49
+ *  49 → 42 + 92 = 97
+ *  97 → 92 + 72 = 130
+ *  130 → 12 + 32 + 02 = 10
+ *  10 → 12 + 02 = 1
+ * 7은 행복한 수이다 ☺.
+ * 
+ * 입력: 각 테스트 케이스는 테스트 케이스 번호와 행복한 소수인지 판정해야 할 정수인 M으로 이루어져 있다. (1 ≤ m ≤ 10000).
+ * 
+ * 출력: 각 테스트 케이스마다, 테스트 케이스의 번호, 입력받은 수, 만일 M이 행복한 소수라면 YES 아니라면 NO를 공백으로 각각 구분하여 출력한다.
+ * 
+ */
+
+
+/**
+   4       // 테스트 케이스 수 P
+   1 1     // 케이스 번호 행복한 수소 대상 수 
+   2 7
+   3 383
+   4 1000
+ */
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+const testCaseCount = input.shift();
+const caseGroups = input.map((caseGroup) => caseGroup.split(" "));
+const testArr = [[1,1],[2,7],[3,383],[4,1000]];
+
+// 소수 판정함수.
+const isPrime = (target) => {
+    if(target === 1) return false;
+    for (let i = 2; i * i <= target; i++) {
+        if(target % i === 0 ) return false;        
+    };
+    return true;
+};
+
+const isHappyPrime = (target,maximun) => {
+    if(target === 1 || maximun === 5) return false;
+    const targetToStringArr = [...target+""]; 
+    const sumOfSquares = targetToStringArr.reduce((p,c) => p + c * c, 0);
+    return isHappyPrime(sumOfSquares,maximun++);
+};
+
+
+const prime10434 = (target) => {
+    if(target === 1 || !isPrime(target)) return "NO";
+    return isHappyPrime(target) ? "YES" : "NO";
+};
+
+
+testArr.forEach((caseGroup) => {
+    console.log(`${caseGroup[0]} ${caseGroup[1]} ${prime10434(+caseGroup[1])}`);
+});
